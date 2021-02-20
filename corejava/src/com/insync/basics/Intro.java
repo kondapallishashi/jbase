c831b6b26a042676a088e4c5d8a32baba86950b7
Sasidhar R Kondapalli <kondapallishashi@gmail.com>
2021-02-20 13:12:58 +0530

    Initial Commit

---
+++ corejava/src/com/insync/basics/Intro.java
@@ -0,0 +1,14 @@
+//This is the declaration of a package and is optional at this stage.
+package com.insync.basics;
+
+//This class demonstrates the following concepts:
+//usage of single line, multi-line comments
+//Basic class usage with main method and printing a line of text
+
+
+public class Intro {
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome to the world of Java");
+	}
+}
