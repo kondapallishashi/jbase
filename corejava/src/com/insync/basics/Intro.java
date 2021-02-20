3be809ab101e2403e882ea9ed7a6c77c944571c7
Sasidhar R Kondapalli <kondapallishashi@gmail.com>
2021-02-20 13:39:50 +0530

    Update1

--- corejava/src/com/insync/basics/Intro.java
+++ corejava/src/com/insync/basics/Intro.java
@@ -5,8 +5,9 @@ package com.insync.basics;
 //usage of single line, multi-line comments
 //Basic class usage with main method and printing a line of text
 
-
+//Define and declare a public class with name Intro.
 public class Intro {
+	//The main method denotes the entry point of the program
 	public static void main(String[] args) {
 	
 		System.out.println("Welcome to the world of Java");
